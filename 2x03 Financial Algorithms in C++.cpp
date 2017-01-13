8f832baf7d7caa33f83f5b94b3289ec1cdcfb63c
James Piper <github@jamespiper.com>
2017-01-13 11:56:58 -0500

    Starting Point
    Blank project as the starting point.
    Windows console app using Visual Studio 2015.
    The objective is to create various financial functions that can be
    called as needed in other projects.
    This is a testing and creation environment.

---
+++ 2x03 Financial Algorithms in C++.cpp
@@ -0,0 +1,21 @@
+//==============================================================================
+// (c) 2017.01.12 by James Piper, james@jamespiper.com
+// 
+// 
+// 
+//==============================================================================
+
+// 2x03 Financial Algorithms in C++.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	// Test using Github
+	int num = 4000;
+	printf("Here is a number %d", num);
+	return 0;
+}
+
